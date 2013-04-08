f7a7fcc8593ec65b93eee1c837e34807185d69b4
Joe Koberg <joe.koberg@versionone.com>
2013-04-08 17:35:53 -0400

    New random number function and WIP vanity key handling

---
+++ FCoin/Random.fs
@@ -0,0 +1,8 @@
+﻿module Random
+
+let Rng = System.Security.Cryptography.RNGCryptoServiceProvider.Create()
+
+let bytes count =
+  let mutable out:byte[] = Array.zeroCreate count
+  do Rng.GetBytes(out)
+  out
---
+++ FCoin/Vanity.fs
@@ -0,0 +1,9 @@
+﻿module Vanity
+
+let genPrivkey = 
+  let random = Radix.BigEndian.toBigInt (Random.bytes 256)
+  let pub = EcDsa.secp256k1.multiply random EcDsa.secp256k1.G
+  Base58.toBase58check 0uy [||]
+
+
+  
