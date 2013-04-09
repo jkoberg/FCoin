72d543486559a824e0a2222612872ab19171fc9e
Joe Koberg <joe.koberg@versionone.com>
2013-04-09 12:35:57 -0400

    cleanup main program

--- VanityTool/Program.fs
+++ VanityTool/Program.fs
@@ -6,8 +6,15 @@ open EcDsa.Arith
 
 [<EntryPoint>]
 let main argv = 
-    let step_1_privkey = EcDsa.secp256k1.newPrivKey()
-    let step_1_pubkey = EcDsa.secp256k1.getPubKey step_1_privkey 
+    let step_1_privkey =
+      if argv.Length > 0
+      then argv.[0] |> Conv.UnsignedBig.fromHex
+      else EcDsa.secp256k1.newPrivKey()
+
+    printfn "Your step 1 private key: %s" (step_1_privkey |> Conv.UnsignedBig.toHex)
+
+    let step_1_pubkey = EcDsa.secp256k1.getPubKey step_1_privkey
+
     let step_2_pubkey_for_pool = 
       match step_1_pubkey with 
       | PointO -> failwith "Somehow got pointO"
@@ -21,10 +28,12 @@ let main argv =
     printfn "Give pool this pubkey: %s" (step_2_pubkey_for_pool |> Conv.Hex.fromBytes)
 
     printfn "Paste back the solution: "
-    let x = System.Console.ReadLine()
-    let solution_privkey = Conv.UnsignedBig.fromHex x
-    let new_privkey = step_1_privkey + solution_privkey % EcDsa.secp256k1.n
+    let step_3_solution_privkey = System.Console.ReadLine() |> Conv.UnsignedBig.fromHex
+
+    // elliptic curve addition and reduction by the generator order
+    let secret_vanity_privkey = (step_1_privkey + step_3_solution_privkey) % EcDsa.secp256k1.n
+
     printfn "Your new private key: %s" (Conv.Bitcoin.toWalletImportFormat secret_vanity_privkey)
     printfn "And Address: %s" (Conv.Bitcoin.toAddressFormat secret_vanity_privkey)
 
-    0
+    0 // program return code to OS
