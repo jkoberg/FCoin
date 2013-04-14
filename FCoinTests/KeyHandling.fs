b3d8684e0aef9e0ad3eb2c3eb076a3ce408791df
Joe Koberg <joe.koberg@versionone.com>
2013-04-14 13:36:34 -0400

    some additional tests for compressed pubkeys

---
+++ FCoinTests/KeyHandling.fs
@@ -0,0 +1,31 @@
+﻿module KeyHandling
+
+open NUnit.Framework
+
+module KeyHandlingTests = 
+  open EcDsa.secp256k1
+  open Conv.Bitcoin
+
+  let compressedHex = ""
+
+
+  let [<Test>] fromCompressed () =
+    let privkey, pubkey, r = newKeypair()
+
+    let uncompressed = toUncompressedPubKey pubkey
+    let newpub2 = fromEncodedPubkey uncompressed
+    Assert.AreEqual(pubkey, newpub2, "didn't encode or decode to uncompressed correctly")
+
+    let compressed = toCompressedPubkey pubkey
+    let newpub1 = fromEncodedPubkey compressed
+    if newpub1 <> pubkey
+      then Assert.Fail(sprintf "didn't decode %s\nto %s\ninstead got %s"
+                          (compressed |> Conv.Hex.fromBytes)
+                          (pubkey |> toUncompressedPubKey |> Conv.Hex.fromBytes)
+                          (newpub1 |> toUncompressedPubKey |> Conv.Hex.fromBytes)
+          )
+      else Assert.Pass(sprintf "unpacked compressed pubkey %s to %s" (compressed |> Conv.Hex.fromBytes) (newpub1 |> toUncompressedPubKey |> Conv.Hex.fromBytes))
+
+
+
+        
---
+++ FCoinTests/SignatureTests.fs
@@ -0,0 +1,27 @@
+﻿module SignatureTests
+
+open NUnit.Framework
+
+open Conv.Bytes
+ 
+
+module SignatureTests = 
+  open EcDsa.secp256k1
+
+  let myPrivkey, myPubkey, r = newKeypair()
+  let message = "Hello There!"B
+  let hash = Crypto.sha256 message |> Conv.UnsignedBig.fromBytes
+
+  let [<Test>] sign () =
+    let r,s = sign myPrivkey hash
+    match verify myPubkey hash (r,s) with
+    | EcDsa.Error msg -> Assert.Fail(msg)
+    | EcDsa.Valid payload -> Assert.Pass(sprintf "good payload of hash %A" (payload))
+    
+
+  let [<Test>] example1 () =
+    let addr = "1joekaZPA1QZtg8kH99Fak4FP6JZ17nZi"
+    let msg = "Hello There!"B
+    let sigstr = "G9XC1ARSjDSXtXf2c1UDpWd+1ghkHPANnClKi83dIKNYBTxvrkDJEmnkLXWwrg9T9rClZztpQ89uH2Nn08c6BYQ="
+    let sigbytes = System.Convert.FromBase64String sigstr
+    None
