a14268ac81ff835eac929a9d819abc736c95737d
Joe Koberg <joe.koberg@versionone.com>
2013-04-14 14:25:05 -0400

    move generic functions out of secp256k1 module and parameterize; cleanup

--- FCoin/EcDsa.fs
+++ FCoin/EcDsa.fs
@@ -1,9 +1,12 @@
 ﻿module EcDsa
 
 type Point = PointO | Point of  bigint * bigint
-type Curve = {p: bigint; a: bigint;  b: bigint}
+type Curve = {a:bigint; b:bigint; p:bigint; G:Point; n:bigint; h:bigint; size:int}
 type PrivateKey = bigint
 type PublicKey = Point
+type MaybeError<'a> = 
+      | Valid of 'a
+      | Error of string
 
 let (%) x m = if x < 0I then (x % m) + m else x % m
 
@@ -26,8 +29,7 @@ let double (c: Curve) p =
     | PointO -> PointO
     | Point(x, y) ->
         let s = modDiv ((3I * (x ** 2)) + c.a) (2I * y) c.p
-        let newx = ((s ** 2) - (2I * x)) % c.p // was working
-        //let newx = ((l ** 2) - c.a - (2I * x)) % c.p // wikipedia
+        let newx = ((s ** 2) - (2I * x) - c.a) % c.p // wikipedia
         let newy = ((s * (x - newx)) - y) % c.p
         Point(newx, newy)
 
@@ -40,8 +42,7 @@ let add (c: Curve) p1 p2 =
     | Point(x1, y1), Point(x2, y2) when x1 = x2 -> PointO ///??? //failwith "How to divide by 0???"
     | Point(x1, y1), Point(x2, y2) ->
         let s = modDiv ((y1 - y2) % c.p) ((x1 - x2) % c.p) c.p
-        let newx = ((s ** 2) - x1 - x2) % c.p // was working, http://www.johannes-bauer.com/compsci/ecc/
-        //let newx = ((l ** 2) - c.a - x1 - x2) % c.p // wikipedia
+        let newx = ((s ** 2) - c.a - x1 - x2) % c.p // wikipedia
         let newy = ((s * (x1 - newx)) - y1) % c.p
         Point(newx, newy)
 
@@ -65,9 +66,47 @@ let fromCompressed (c:Curve) isYOdd x : PublicKey =
       then Point(x, c.p - ytrial)
       else Point(x, ytrial)
 
-type MaybeError<'a> = 
-      | Valid of 'a
-      | Error of string
+
+let rec newPrivKey (c:Curve) : PrivateKey = 
+    let r = Crypto.randBits c.size
+    if r > 0I && r < c.n then r else newPrivKey c
+
+
+let getPubKey c k : PublicKey = multiply c k c.G
+
+let rec newKeypair c =
+    let k = newPrivKey c
+    match getPubKey c k with
+    | PointO -> newKeypair c
+    | Point(r,_) when r % c.n = 0I -> newKeypair c
+    | Point(r,_) as kG -> (k, kG, r)
+
+let rec sign (c:Curve) privkey hash = 
+    let z = hash
+    let k, kG, r = newKeypair c
+    let s = ((modInv k c.n) * ((z + ((r * privkey)%c.n))%c.n) ) % c.n
+    if s = 0I then sign c privkey hash else
+    (r, s)
+        
+let verify (c:Curve) pubkey hash (r,s) =
+  let outOfRange i = i < 1I || i > c.n
+  if outOfRange r || outOfRange s then Error "Invalid signature" else 
+  match pubkey with
+  | PointO -> Error "invalid pubkey"
+  | pubkey when not (onCurve c pubkey) -> Error "invalid pubkey"
+  | pubkey when (multiply c c.n pubkey) <> PointO -> Error "invalid pubkey"
+  | pubkey ->
+    let z = hash
+    let w = modInv s c.n
+    let u1 = (z * w) % c.n
+    let u2 = (r * w) % c.n
+    match add c (multiply c u1 c.G) (multiply c u2 pubkey) with
+    | PointO -> Error "Signature didn't match"
+    | Point(x1, _) when r <> (x1 % c.n) -> Error "Signature didn't match"
+    | _ -> Valid hash
+
+
+
 
 module secp256k1 =
     open Conv.UnsignedBig
@@ -80,57 +119,15 @@ module secp256k1 =
                   fromHex "483ADA77 26A3C465 5DA4FBFC 0E1108A8 FD17B448 A6855419 9C47D08F FB10D4B8")
     let n = fromHex "FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE BAAEDCE6 AF48A03B BFD25E8C D0364141"
     let h = 1I
-    let curve = {a = a; b = b; p = p}
-
+    let curve = {a=a; b=b; p=p; G=G; n=n; h=h; size=size}
 
     let double = double curve
     let add = add curve
     let multiply = multiply curve
     let onCurve = onCurve curve
     let fromCompressed = fromCompressed curve
-
-    let ( @+ ) = add
-    let ( @* ) = multiply
-
-    let getPubKey privkey : PublicKey = privkey @* G
-
-    let rec newPrivKey () : PrivateKey = 
-      let r = Crypto.randBits size
-      if r > 0I && r < n then r else newPrivKey()
-
-    let rec newKeypair() =
-        let k = newPrivKey()
-        match getPubKey k with
-        | PointO -> newKeypair()
-        | Point(r,_) when r % n = 0I -> newKeypair()
-        | Point(r,_) as kG -> (k, kG, r)
-
-    let checkPubkey pubkey = 
-        match pubkey with
-        | PointO -> Error "invalid pubkey"
-        | pubkey when not (onCurve pubkey) -> Error "invalid pubkey"
-        | pubkey when (n @* pubkey) <> PointO -> Error "invalid pubkey"
-        | _ -> Valid pubkey
-
-    let rec sign privkey hash = 
-        let z = hash
-        let k, kG, r = newKeypair()
-        let s = ((modInv k n) * ((z + ((r * privkey)%n))%n) ) % n
-        if s = 0I then sign privkey hash else
-        (r, s)
-        
-    let outOfRange i = i < 1I || i > n
-
-    let verify pubkey hash (r,s) =
-        if outOfRange r || outOfRange s then Error "Invalid signature" else 
-        match checkPubkey pubkey with
-        | Error msg -> Error msg
-        | Valid pubkey ->
-            let z = hash
-            let w = modInv s n
-            let u1 = (z * w) % n
-            let u2 = (r * w) % n
-            match (u1 @* G) @+ (u2 @* pubkey) with
-            | PointO -> Error "Signature didn't match"
-            | Point(x1, _) when r <> (x1 % n) -> Error "Signature didn't match"
-            | _ -> Valid hash
+    let newPrivKey () = newPrivKey curve
+    let getPubKey = getPubKey curve
+    let newKeypair () = newKeypair curve
+    let sign = sign curve
+    let verify = verify curve
