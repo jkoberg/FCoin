21e7ddd5b9922646d0488e5d1eff2c6ce62607ff
Joe Koberg <joe.koberg@versionone.com>
2013-04-14 15:35:37 -0400

    debug output

--- FCoinTests/KeyHandling.fs
+++ FCoinTests/KeyHandling.fs
@@ -19,10 +19,12 @@ module KeyHandlingTests =
     let compressed = toCompressedPubkey pubkey
     let newpub1 = fromEncodedPubkey compressed
     if newpub1 <> pubkey
-      then Assert.Fail(sprintf "didn't decode %s\nto %s\ninstead got %s"
+      then Assert.Fail(sprintf "didn't decode %s\nto %s\ninstead got %s\n privkey %s"
                           (compressed |> Conv.Hex.fromBytes)
                           (pubkey |> toUncompressedPubKey |> Conv.Hex.fromBytes)
                           (newpub1 |> toUncompressedPubKey |> Conv.Hex.fromBytes)
+                          (privkey |> Conv.UnsignedBig.toHex)
+
           )
       else Assert.Pass(sprintf "unpacked compressed pubkey %s to %s" (compressed |> Conv.Hex.fromBytes) (newpub1 |> toUncompressedPubKey |> Conv.Hex.fromBytes))
 
